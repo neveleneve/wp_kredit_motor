04a79cee6c5e06961b3dbd3bdb1c136ea0753d64
admin <admin@MacBook-Pro.local>
2021-03-17 19:54:59 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Login;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -10,9 +13,42 @@ class LoginController extends Controller
     {
         return view('login');
     }
-    
+
     public function loggingin(Request $data)
     {
-        dd($data->all());
+        $datalogin = Login::where('username', $data->username)->get();
+        if (count($datalogin) <= 0) {
+            // Username tidak ditemukan
+            return redirect(route('login'))
+                ->with('infousername', 'Nama pengguna tidak ditemukan')
+                ->with('color', 'danger');
+        } else {
+            // Username ditemukan
+            if (Hash::check($data->password, $datalogin[0]['password'])) {
+                // Berhasil login
+                if ($datalogin[0]['level'] == 0) {
+                    Auth::guard('cs')->LoginUsingId($datalogin[0]['id']);
+                    return redirect(route('csdashboard'));
+                }elseif ($datalogin[0]['level'] == 1) {
+                    Auth::guard('marketing')->LoginUsingId($datalogin[0]['id']);
+                    return redirect(route('dashboard'));
+                }
+            } else {
+                // Password Salah
+                return redirect(route('login'))
+                    ->with('infousername', 'Password salah')
+                    ->with('color', 'danger');
+            }
+        }
+    }
+
+    public function logout()
+    {
+        if (Auth::guard('marketing')->check()) {
+            Auth::guard('marketing')->logout();
+        } elseif (Auth::guard('cs')->check()) {
+            Auth::guard('cs')->logout();
+        }
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,40 +5,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'LoginController@index')->name('login');
 Route::get('/credit-check', 'KreditController@index')->name('credit-check');
 Route::post('/login', 'LoginController@loggingin')->name('loggingin');
-Route::get('/logout', 'MarketingController@logout')->name('logout');
+Route::get('/logout', 'LoginController@logout')->name('logout');
 
 #region MARKETING
+Route::group(['middleware' => 'auth:marketing'], function () {
+    #region MARKETING-DASHBOAR
+    Route::get('/marketing', 'MarketingController@dashboard')->name('dashboard');
+    #endregion
 
-#region MARKETING-DASHBOAR
-Route::get('/marketing', 'MarketingController@dashboard')->name('dashboard');
-#endregion
+    #region MARKETING-NASABAH
+    Route::get('/marketing/nasabah', 'MarketingController@nasabah')->name('nasabah');
+    Route::get('/marketing/nasabah/add', 'MarketingController@addnasabah')->name('addnasabah');
+    Route::post('/marketing/nasabah/add', 'MarketingController@ceknasabah')->name('ceknasabah');
+    Route::get('/marketing/nasabah/view/{id}', 'MarketingController@viewnasabah')->name('viewnasabah');
+    Route::get('/marketing/nasabah/transaksi/{id}', 'MarketingController@transaksinasabah')->name('transaksinasabah');
+    #endregion
 
-#region MARKETING-NASABAH
-Route::get('/marketing/nasabah', 'MarketingController@nasabah')->name('nasabah');
-Route::get('/marketing/nasabah/add', 'MarketingController@addnasabah')->name('addnasabah');
-Route::post('/marketing/nasabah/add', 'MarketingController@ceknasabah')->name('ceknasabah');
-Route::get('/marketing/nasabah/view/{id}', 'MarketingController@viewnasabah')->name('viewnasabah');
-Route::get('/marketing/nasabah/transaksi/{id}', 'MarketingController@transaksinasabah')->name('transaksinasabah');
-#endregion
+    #region MARKETING-KREDIT
+    Route::get('/marketing/kredit', 'MarketingController@kredit')->name('kredit');
+    Route::get('/marketing/kredit/add', 'MarketingController@addkredit')->name('addkredit');
+    Route::get('/marketing/kredit/view/{id}', 'MarketingController@viewkredit')->name('viewkredit');
+    #endregion
 
-#region MARKETING-KREDIT
-Route::get('/marketing/kredit', 'MarketingController@kredit')->name('kredit');
-Route::get('/marketing/kredit/add', 'MarketingController@addkredit')->name('addkredit');
-Route::get('/marketing/kredit/view/{id}', 'MarketingController@viewkredit')->name('viewkredit');
-#endregion
+    #region MARKETING-PAYMENT
+    Route::get('/marketing/payment', 'MarketingController@payment')->name('payment');
+    Route::get('/marketing/payment/add', 'MarketingController@addpayment')->name('addpayment');
+    #endregion
 
-#region MARKETING-PAYMENT
-Route::get('/marketing/payment', 'MarketingController@payment')->name('payment');
-Route::get('/marketing/payment/add', 'MarketingController@addpayment')->name('addpayment');
-#endregion
-
-#region MARKETING-SETTING
-Route::get('/marketing/setting', 'MarketingController@setting')->name('setting');
-#endregion
-
+    #region MARKETING-SETTING
+    Route::get('/marketing/setting', 'MarketingController@setting')->name('setting');
+    #endregion
+});
 #endregion
 
 #region CS
+
+Route::get('/cs', 'CSController@setting')->name('csdashboard');
 Route::get('/cs/setting', 'MarketingController@setting')->name('cssetting');
 
 #endregion
