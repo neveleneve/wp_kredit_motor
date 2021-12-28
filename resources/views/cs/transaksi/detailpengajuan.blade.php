04b330fd6199ad1ee91b9d11a3f98944c40bcbf1
neveleneve <pandupandu813@gmail.com>
2021-12-29 00:24:49 +0700

    update latest

--- resources/views/cs/transaksi/detailpengajuan.blade.php
+++ resources/views/cs/transaksi/detailpengajuan.blade.php
@@ -11,6 +11,7 @@
     <link rel="icon" href="{{ asset('penyimpanan/logo/alco-square.jpg') }}">
     <title>Cetak Detail Pengajuan</title>
     <link href="{{ asset('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('template/css/style.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="{{ asset('template/css/sb-admin-2.css') }}" rel="stylesheet">
 </head>
@@ -406,7 +407,20 @@
                 </table>
             </div>
         </div>
+        <a class="scroll-to-top rounded" href="javascript:window.print()">
+            <i class="fa fa-print"></i>
+        </a>
+        {{-- <div class="row mb-3 noprint">
+            <div class="col-2">
+                <a class="btn btn-primary btn-block" href="javascript:window.print()">Cetak</a>
+            </div>
+        </div> --}}
     </div>
+    <script src="{{ asset('template/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('template/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('template/js/sb-admin-2.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/ajax.js') }}"></script>
 </body>
 
 </html>
